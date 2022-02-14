55bede5e208419e0dae2e14b4bb7da537d91be51
sanjipun <iamsanjipun@gmail.com>
2022-02-14 20:55:15 +0545

    send tokens to 2 different addresses

---
+++ send-token-to-2-addresses.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract sendTokenTo2Addresses {
+    using SafeMath for uint256;
+}
